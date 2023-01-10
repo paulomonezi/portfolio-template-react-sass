811ee15b78f6a4c3e6f768fd73f738042489d596
Paulo Monezi <paulo.monezi@icloud.com>
2023-01-10 13:48:15 -0400

    feat(projects): implemented projects container

---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,18 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum eaque
+        blanditiis molestias consequatur maxime reiciendis dolor tempora
+        distinctio nam aperiam. Porro deserunt doloremque facilis magni vitae
+        mollitia consequuntur accusantium accusamus?
+      </p>
+      <a href="" className="btn">
+        Ver projeto
+      </a>
+    </section>
+  )
+}
+
+export default ProjectsContainer
