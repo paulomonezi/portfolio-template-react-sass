72c57281e1d788b553d996ae752a3604285c3d13
Paulo Monezi <paulo.monezi@icloud.com>
2023-01-10 13:47:18 -0400

    feat(about): implemented about container

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,17 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        rerum nam animi harum tempore. Deserunt, non neque. Sunt ex dignissimos
+        nesciunt ipsa, eveniet perspiciatis dicta itaque officiis beatae eum
+        doloribus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quas iure velit ducimus nesciunt tempore similique perspiciatis ex quidem voluptatibus nulla eaque temporibus sed libero quaerat consectetur doloremque. Quaerat animi molestias in aperiam neque numquam eius, soluta excepturi eveniet accusamus quam expedita quo quibusdam, ullam beatae earum voluptatum nihil nemo atque recusandae totam ratione dolor fugiat. Temporibus, distinctio voluptatum. Reiciendis fugiat repellat voluptatum dolor assumenda ducimus doloremque cupiditate rerum illum laborum, impedit minima ea labore commodi voluptatibus illo sapiente iusto necessitatibus porro minus blanditiis voluptates? Nisi, quidem voluptas possimus dolore qui rem mollitia nihil inventore dignissimos ducimus quas repudiandae nostrum!
+      </p>
+      
+    </section>
+  )
+}
+
+export default AboutContainer
