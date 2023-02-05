ea39afbf148437fff427e0cd0c603c62fb2d237d
Paulo Monezi <paulo.monezi@icloud.com>
2023-02-04 20:20:36 -0400

    feat: added theme toggler via theme context

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,20 @@
-import '../styles/components/maincontent.sass'
+import { useContext } from "react"
+import { ThemeContext } from "../contexts/ThemeContext.jsx"
 
-import AboutContainer from './AboutContainer'
-import ProjectsContainer from './ProjectsContainer'
-import TechsContainer from './TechsContainer'
+import "../styles/components/maincontent.sass"
+
+import AboutContainer from "./AboutContainer"
+import ProjectsContainer from "./ProjectsContainer"
+import TechsContainer from "./TechsContainer"
 
 const MainContent = () => {
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <main id="main-content">
-      <AboutContainer/>
-      <TechsContainer/>
-      <ProjectsContainer/>
+    <main id="main-content" style={{background: theme.bgColor}}>
+      <AboutContainer />
+      <TechsContainer />
+      <ProjectsContainer />
     </main>
   )
 }
